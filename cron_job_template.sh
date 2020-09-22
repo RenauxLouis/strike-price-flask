7da38a291fb027c6bff7bc52733164c0c4f14f75
RenauxLouis <renauxlouis@gmail.com>
2020-09-21 21:23:46 -0400

    add cron template

--- cron_job_template.sh
+++ cron_job_template.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export MAIL_USERNAME = "your.email@gmail.com"
-export MAIL_PASSWORD = "your.password"
+export MAIL_USERNAME = <your.email>
+export MAIL_PASSWORD = <your.password>
 cd /home/ubuntu/workspace/investing && /home/ubuntu/.local/bin/flask users strike_price_compare >> scheduled.log 2 > &1
