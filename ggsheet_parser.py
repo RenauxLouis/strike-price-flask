3e30a27228f0aa93a48600d7fe4f172279131a4d
RenauxLouis <renauxlouis@gmail.com>
2020-11-28 18:09:05 -0500

    clean ggsheet parsing

--- ggsheet_parser.py
+++ ggsheet_parser.py
@@ -1,15 +1,15 @@
 from __future__ import print_function
 
+import os
 from datetime import date
-import pandas as pd
+from decimal import Decimal
+from re import sub
+
 import gspread
+import numpy as np
+import pandas as pd
 
 from constants import CSV_FPATH
-
-from re import sub
-from decimal import Decimal
-import numpy as np
-
 
 ALL_TICKERS_CSV_FPATH = "all_tickers.csv"
 
@@ -24,19 +24,24 @@ def get_ggsheet_as_df():
 
     wks = connect_to_worksheet()
     df = pd.DataFrame(wks.get_all_records())
+    local_df_columns = ["ticker", "strike_price"]
     if not df.empty:
-        df = df.rename(columns={"TICKER": "ticker",
-                                "STRIKE PRICE": "strike_price"})
+        clean_columns_rename = {col: col.replace(" ", "") for col in
+                                df.columns}
+        df = df.rename(columns=clean_columns_rename)
+        df = df.rename(columns={
+            "TICKER": "ticker", "STRIKEPRICE": "strike_price"})
         df["ticker"].replace("", np.nan, inplace=True)
         df["strike_price"].replace("", np.nan, inplace=True)
         df = df.dropna()
+
+        df = df[local_df_columns]
 
         all_tickers = pd.read_csv(
             ALL_TICKERS_CSV_FPATH, index_col=False)["ticker"]
         df = df.loc[df["ticker"].isin(all_tickers)]
         df["strike_price"] = df["strike_price"].apply(
-            lambda x: Decimal(sub(r"[^\d.]", "", x)))
-        df = df.set_index("")
+            lambda x: float(sub(r"[^\d.]", "", x)))
 
     return df
 
@@ -50,8 +55,6 @@ def get_new_tickers_and_prices(new_df, df_existing):
         ticker_price for ticker_price in new_tickers_prices if (
             ticker_price not in existing_tickers_prices)]
 
-    print(not_preexisting_tuples)
-
     new_tickers = [ticker for ticker, _ in not_preexisting_tuples]
 
     return new_tickers
@@ -59,13 +62,15 @@ def get_new_tickers_and_prices(new_df, df_existing):
 
 def get_df_with_dates():
     new_df = get_ggsheet_as_df()
-    df_existing = pd.read_csv(CSV_FPATH, index_col=False)
+    if os.path.exists(CSV_FPATH):
+        df_existing = pd.read_csv(CSV_FPATH, index_col=False)
+    else:
+        df_existing = pd.DataFrame(columns=new_df.columns)
 
     if new_df.empty:
         df_updated = df_existing
     else:
         new_tickers = get_new_tickers_and_prices(new_df, df_existing)
-
         df_new_tickers = new_df.loc[new_df["ticker"].isin(new_tickers)].copy()
         df_new_tickers["strike_price_query_date"] = str(date.today())
         df_updated = df_existing.append(df_new_tickers)
